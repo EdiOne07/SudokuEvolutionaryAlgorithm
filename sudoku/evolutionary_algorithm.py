92e511cebee3b890ecec472a5bbeba3e5adcb491
iM0ritz <moritz.schlitter@kabelmail.de>
2025-09-20 11:29:36 +0200

    Start of the implemenation of creating the generations

--- sudoku/evolutionary_algorithm.py
+++ sudoku/evolutionary_algorithm.py
@@ -1,5 +1,6 @@
 import sudoku.boards as boards
 from sudoku.sudoku import Sudoku
+import heuristic
 import random
 
 
@@ -10,6 +11,7 @@ class SudokuEvolutionaryAlgorithm:
     _mutation_rate: float
     _generations: int
     _population: list[Sudoku]
+    _solution: Sudoku
 
     def __init__(self, initial_board: Sudoku, population_size: int = 100, mutation_rate: float = 0.01):
         """Initializes the evolutionary algorithm class.
@@ -27,6 +29,7 @@ class SudokuEvolutionaryAlgorithm:
         self._population_size = population_size
         self._mutation_rate = mutation_rate
         self._generations = 0
+        self._solution = None
 
     def __initialize_population(self):
         """Creates the initial population for generation 0."""     
@@ -37,6 +40,7 @@ class SudokuEvolutionaryAlgorithm:
                     if randomly_filled_board[i][j] == 0:
                         randomly_filled_board[i][j] = random.randint(1, 9)
 
+            randomly_filled_board.update_score()
             self._population.append(randomly_filled_board)
 
     def __solution_found(self) -> bool:
@@ -49,8 +53,29 @@ class SudokuEvolutionaryAlgorithm:
         """
         for individual in self._population:
             if individual.check_goal_state():
+                self._solution = individual
                 return True
         return False
+    
+    def __create_next_generation(self) -> bool:
+        """Creates the next generation of the population.
+        
+        Returns
+        -------
+        bool
+            Whether the next generation was created.
+            If false is returned, a solution has been found.
+        """
+        if self.__solution_found():
+            return False
+
+        # Selection
+        # Top 20% of population creates 80% of next generation
+        # 20% of the all population creates 20% of the next generation
+        self._population.sort(key=lambda x: x.get_score(), reverse=True)   
+        
+        return True           
+
 
     def solve(self) -> Sudoku:
         """Solves the sudoku puzzle using an evolutionary algorithm."""
