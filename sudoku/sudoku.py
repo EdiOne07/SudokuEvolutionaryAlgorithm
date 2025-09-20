389ece809ccb616b0ff27f235d699c58c2493057
iM0ritz <moritz.schlitter@kabelmail.de>
2025-09-20 10:58:10 +0200

    Add a class for a Sudoku board

---
+++ sudoku/sudoku.py
@@ -0,0 +1,64 @@
+class Sudoku:
+    """A class to represent a Sudoku board."""
+    _board: list[list[int]]
+
+    def __init__(self, board: list[list[int]]):
+        """Initializes a new instance of a Sudoku board.
+        
+            Parameters
+            ----------
+            board: list[list[int]]
+                The sudoku board to use.
+        """
+        self._board = board
+
+    def get_board(self) -> list[list[int]]:
+        """Returns the current state of the board.
+        
+        Returns
+        -------
+        list[list[int]]
+            The current state of the board.
+        """
+        return self._board
+    
+    def set_board(self, board: list[list[int]]):
+        """Sets the current state of the board.
+        
+            Parameters
+            ----------
+            board: list[list[int]]
+                The new state of the board.
+        """
+        self._board = board
+
+    def check_goal_state(self) -> bool:
+        """Check whether the current state is a goal state.
+
+        Returns
+        -------
+        bool
+            Whether the current state is a goal state."""
+        
+        # Check rows
+        for row in self.board:
+            if sum(row) != 45:
+                return False
+        
+        # Check columns
+        for j in range(9):
+            column_sum = sum(self.board[i][j] for i in range(9))
+            if column_sum != 45:
+                return False
+        
+        # Check 3x3 boxes
+        for box_row in range(3):
+            for box_col in range(3):
+                box_sum = 0
+                for i in range(3):
+                    for j in range(3):
+                        box_sum += self.board[box_row * 3 + i][box_col * 3 + j]
+                if box_sum != 45:
+                    return False
+        
+        return True
